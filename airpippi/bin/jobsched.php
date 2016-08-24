cf3f7132af54b7072a06d4614759c197ab5551c4
Akira Ouchi <akkiesoft@marokun.net>
2016-08-25 07:36:23 +0900

    ジョブスケジューラを追加
    JSONに記述することでプログラムタイマー的に
    実行できる仕組みを追加。
    cronでもできるけど、Webから操作できるようにしたいので
    何かしらの仕組みを噛ますことにした。

---
+++ airpippi/bin/jobsched.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once '/opt/airpippi/config.php';
+require_once '/opt/airpippi/bin/rungpio.php';
+
+$json_raw = file_get_contents("/opt/airpippi/joblist.json");
+$json = json_decode($json_raw);
+
+foreach ($json->list as $item) {
+	# is enabled?
+	if (! $item->enabled) continue;
+
+	# check day of week
+	$now_dow = strtolower(date("D"));
+	if (! $item->dow->$now_dow) continue;
+
+	# check hour and minute
+	$jobtime = array(intval($item->hour), intval($item->min));
+	$now = array(intval(date("G")), intval(date("i")));
+	if ($jobtime !== $now) continue;
+
+	# run a job
+	$job = $item->task;
+	switch($job){
+	case 0:
+		# power
+		runGPIO();
+		break;
+	}
+}
