31864beb0de48c2c15197b2bddbf519b427f87c4
Akira Ouchi <akkiesoft@marokun.net>
2016-09-21 01:33:15 +0900

    jQueryがcdn頼りなの完全にアホだった

--- airpippi/www/settings.php
+++ airpippi/www/settings.php
@@ -173,7 +173,7 @@ if ($pin == "" && ! isset($data['airpippi_twit'])) {
     </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/jquery-2.2.4.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
   </body>
